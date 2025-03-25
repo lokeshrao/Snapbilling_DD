9c2f5d6e376274f1ad0413b280a71e39785d8be5
lokesh.y <lokesh.y@snapbizz.com>
2025-03-25 12:41:34 +0530

    minor fix

--- inventory/src/main/java/com/snapbizz/inventory/screen/GlobalDbRepository.kt
+++ inventory/src/main/java/com/snapbizz/inventory/screen/GlobalDbRepository.kt
@@ -8,11 +8,17 @@ import com.snapbizz.core.database.entities.global.GlobalProduct
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GlobalDbRepository @Inject constructor(val snapGlobalDatabase: SnapGlobalDatabase) {
-    fun getProducts(query: String): Flow<PagingData<GlobalProduct>> {
+class GlobalDbRepository @Inject constructor(val snapGlobalDatabase: SnapGlobalDatabase) :
+    IGlobalDbRepository {
+    override fun getProducts(query: String): Flow<PagingData<GlobalProduct>> {
         return Pager(
             config = PagingConfig(pageSize = 10, enablePlaceholders = false),
-            pagingSourceFactory = { snapGlobalDatabase.globalProductDao().getProducts(query) }
-        ).flow
+            pagingSourceFactory = {
+                snapGlobalDatabase.globalProductDao().getProducts(query)
+            }).flow
     }
+}
+
+interface IGlobalDbRepository {
+    fun getProducts(query: String): Flow<PagingData<GlobalProduct>>
 }
